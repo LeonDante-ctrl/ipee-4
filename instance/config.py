bc13d78997ef8bcf80b0a06eaa15dc2dba9d7eee
LeonDante-ctrl <maxgamerdu@gmail.com>
2021-03-11 07:47:19 +0300

    added text into my configurations file as well as email and models into my

---
+++ instance/config.py
@@ -0,0 +1,6 @@
+SECRET_KEY = '\xb33\x9d&\xa9\x13f\xe3\xec21\x18\xfbw\x98Cm'
+
+MAIL_USERNAME = "lappypasta@gmail.com"
+MAIL_PASSWORD = "dontdomedirtylikethat"
+
+DATABASE_URL = 'postgresql+psycopg2://DANTE:MAXYLOON@localhost/blog'
